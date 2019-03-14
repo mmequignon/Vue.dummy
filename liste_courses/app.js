c59b6a6b927539481bb0d644211df52a02731ea1
MmeQuignon <matthieu@fwzte.xyz>
2019-03-14 16:05:15 +0100

    [IMP] sum prices, delete…

--- liste_courses/app.js
+++ liste_courses/app.js
@@ -2,18 +2,48 @@ var app = new Vue({
     el:"#app",
     data: { 
         new_item: "",
+        item: {
+            title: "",
+            price: 0.0,
+            done: false
+        },
         items: [],
-        id: 0
+        id: 0,
+        budget: 0,
+        budget_exceeded: false
+    },
+    computed: {
+        total_sum: function(){
+            total = this.items.reduce((acc, obj) => acc += parseInt(obj.price), 0);
+            this.budget_exceeded = total > this.budget ? true : false;
+            return total;
+        }
     },
     methods: {
+        store: function(){
+            localStorage.setItem("data", JSON.stringify(this.items));
+        },
+        restore: function(){
+            if (localStorage.getItem("data")){
+                this.items = JSON.parse(localStorage.getItem("data"));
+                localStorage.removeItem("data");
+            }
+        },
         add_item: function(){
-            this.items.push({ 
-                id: this.id,
-                done: false,
-                title: this.new_item 
-            });
-            this.new_item = "";
-            this.id ++;
-        }
+            this.items.push({
+                title: this.item.title,
+                price: this.item.price,
+                done: this.item.done
+            })
+            this.item.title = "";
+            this.store();
+        } ,
+        delete_item: function(index){
+            this.items.splice(index, 1);
+            this.store();
+        },
+    },
+    created: function(){
+        this.restore();
     }
 })
